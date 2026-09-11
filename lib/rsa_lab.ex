1753c4ad6e1736e38a0c4314948a21fd0f262303
Aleksandar Dimov <aleksander.dimov@bbc.co.uk>
2026-09-11 13:07:05 +0100

    Add basic notebooks for backup

---
+++ lib/rsa_lab.ex
@@ -0,0 +1,18 @@
+defmodule RsaLab do
+  @moduledoc """
+  Documentation for `RsaLab`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RsaLab.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rsa_lab_test.exs
@@ -0,0 +1,8 @@
+defmodule RsaLabTest do
+  use ExUnit.Case
+  doctest RsaLab
+
+  test "greets the world" do
+    assert RsaLab.hello() == :world
+  end
+end
